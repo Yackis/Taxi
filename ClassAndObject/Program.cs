411297c8d4cbb751a10a57339d27ce7c78e06d2f
Yackis <Yackis1903@students.amikom.ac.id>
2022-03-24 17:07:35 +0700

    Add project files.

---
+++ ClassAndObject/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaxiApps
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //membuat object taksi
+            Taxi ungu = new Taxi();
+            //pengesetan nilai properties
+            ungu.Name = "bejon";
+            ungu.Status = true;
+            ungu.Passenger = 3;
+
+            //pemanggilan method
+            ungu.TaxiInfo();
+            ungu.PickUpPassenger();
+            ungu.DropOffPassenger();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ClassAndObject/Taxi1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace TaxiApps
+{
+    public class Taxi
+    {
+        //membuat properti
+        public string Name { get; set; }
+        public bool Status { get; set; }
+        public int Passenger { get; set; }
+
+        //membuat method
+        public void TaxiInfo()
+        {
+            Console.WriteLine("Driver Name : {0}", Name);
+            Console.WriteLine("On Duty Status : {0}", Status);
+            Console.WriteLine("Number of Passenger : {0}", Passenger);
+        }
+
+        public void PickUpPassenger()
+        {
+            Console.WriteLine("{0} On Duty picking up passenger", Name);
+        }
+
+        public void DropOffPassenger()
+        {
+            Console.WriteLine("{0} Done droping off passenger", Name);
+        }
+    }
+}
